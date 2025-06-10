bd31eaff25bb9935cfbe47825606adc2227144f0
Harish <harishmagnate@gmail.com>
2025-06-10 16:07:01 -0400

    Update Activity.java

--- src/main/java/com/example/batch/Activity.java
+++ src/main/java/com/example/batch/Activity.java
@@ -1,2 +1,18 @@
 package com.example.batch.model;
-public class Activity {}
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@Document(collection = "activities")
+public class Activity {
+    @Id
+    private String memberId;
+    private String action;
+    private Instant timestamp;
+}
