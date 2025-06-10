5744aff19ad2b23a04230bf9148105fcb79436ea
Harish <harishmagnate@gmail.com>
2025-06-10 16:07:24 -0400

    Update Member.java

--- src/main/java/com/example/batch/Member.java
+++ src/main/java/com/example/batch/Member.java
@@ -1,2 +1,16 @@
 package com.example.batch.model;
-public class Member {}
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "members")
+public class Member {
+    @Id
+    private String id;
+    private String name;
+    private String status;
+    private String policyStatus;
+}
